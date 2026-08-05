c9c73f5124b864796a040b3b33299bc4eb55913c
Sylvain TROPÉE <stropee@campus-eni.fr>
2026-08-05 14:29:11 +0200

    module 6 demo 2 form

--- lib/module_6/demo1.dart
+++ lib/module_6/demo1.dart
@@ -36,14 +36,45 @@ class FormPage extends StatefulWidget {
 }
 
 class _FormPageState extends State<FormPage> {
+  final GlobalKey<FormState> _key = GlobalKey<FormState>();
+  String? name, age, drink, animal;
+  bool isChecked = false;
+
+  TextEditingController nameController = TextEditingController();
+
+  String? validateAge(String? value){
+    if (value == null || value.isEmpty) {
+      return "Le champ est obligatoire";
+    }
+    if (int.parse(value) < 18) {
+      return "Interdit au mineur !";
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: _key,
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
             TextFormField(
+              controller: nameController,
+              // initialValue: "coucou",
+              onSaved: (value){
+                name = value;
+              },
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Le champ est obligatoire";
+                }
+                if (value.length < 3) {
+                  return "Minimum 3 caractères !";
+                }
+                return null;
+              },
               maxLines: 5,
               decoration: InputDecoration(
                 alignLabelWithHint: true,
@@ -56,6 +87,10 @@ class _FormPageState extends State<FormPage> {
             ),
             SizedBox(height: 16),
             TextFormField(
+              onSaved: (value){
+                age = value;
+              },
+              validator: validateAge,
               keyboardType: TextInputType.number,
               decoration: InputDecoration(
                 labelText: "Age",
@@ -66,7 +101,10 @@ class _FormPageState extends State<FormPage> {
               ),
             ),
             SizedBox(height: 16),
-            DropdownButtonFormField(
+            DropdownButtonFormField<String>(
+              onSaved: (value){
+                drink = value;
+              },
               initialValue: "",
               decoration: InputDecoration(labelText: "Boisson Préférée"),
               items: [
@@ -79,16 +117,29 @@ class _FormPageState extends State<FormPage> {
                 DropdownMenuItem(value: "tea", child: Text("Thé")),
                 DropdownMenuItem(value: "mint", child: Text("Vittel Menthe")),
               ],
-              onChanged: (value) {},
+              onChanged: (value) {
+                if(value == "mint"){
+                  nameController.text = "C'est bon la menthe !";
+                }
+              },
             ),
             Row(
               children: [
-                Checkbox(value: false, onChanged: (value) {}),
+                Checkbox(value: isChecked, onChanged: (value) {
+                  setState(() {
+                    isChecked = value!;
+                  });
+                }),
                 Text("Accepter les conditions de votre rédition"),
               ],
             ),
-            RadioGroup(
-              onChanged: (value) {},
+            RadioGroup<String>(
+              groupValue: animal,
+              onChanged: (value) {
+                setState(() {
+                  animal = value;
+                });
+              },
               child: Column(
                 children: [
                   RadioListTile(value: "dog", title: Text("Chien")),
@@ -96,7 +147,13 @@ class _FormPageState extends State<FormPage> {
                 ],
               ),
             ),
-            ElevatedButton(onPressed: (){}, child: Text("Soumettre"))
+            ElevatedButton(onPressed: () {
+              if(_key.currentState!.validate()){
+                print("$name - $age - $drink - $animal - $isChecked");
+                _key.currentState!.save();
+                print("$name - $age - $drink - $animal - $isChecked");
+              }
+            }, child: Text("Soumettre")),
           ],
         ),
       ),
